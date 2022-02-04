96ce5d75023f48bf00fe27484ba23a20f689936a
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-04 19:03:18 -0300

    Add GridView to category

---
+++ lib/screens/category.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: const [],
+    );
+  }
+}
