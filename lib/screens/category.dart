4e61044019ec19a02557e83dc391406503c3d00e
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-06 12:53:02 -0300

    Add padding to GridView

--- lib/screens/category.dart
+++ lib/screens/category.dart
@@ -10,6 +10,7 @@ class Categories extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: const Text("Vamos cozinhar?")),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
             childAspectRatio: 3 / 2,
