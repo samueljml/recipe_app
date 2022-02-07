98b6bc358b4206e7827131e620b0b4f803f82033
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-07 20:27:55 -0300

    Add new primarySwatch color

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
         fontFamily: "Raleway",
         canvasColor: const Color.fromRGBO(255, 254, 299, 1),
         textTheme: ThemeData.light().textTheme.copyWith(
-              headline6:
-                  const TextStyle(fontSize: 20, fontFamily: "RobotoCondensed"),
+              headline6: const TextStyle(
+                fontSize: 20,
+                fontFamily: "RobotoCondensed",
+              ),
             ),
       ),
       home: const Categories(),
