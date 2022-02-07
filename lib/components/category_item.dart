b6320804cd3e2c93c0ff20835e6d6b7115408a84
Samuel Andrade <devsamuelandrade@gmail.com>
2022-02-07 20:29:04 -0300

    Add InkWell to push categories meal screen

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,28 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:recipe_app/models/category.dart';
+import 'package:recipe_app/screens/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem(this.category, {Key? key}) : super(key: key);
 
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => const CategoriesMealsScreen(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
       ),
     );
